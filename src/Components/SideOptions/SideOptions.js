ebc315e193f3990c343c53e6b35c80eb8a26292a
Guillermo Barila <gui365@yahoo.com.ar>
2018-11-07 17:03:46 -0500

    Side options component created

---
+++ src/Components/SideOptions/SideOptions.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SideOptions.css";
+import "../../App.css";
+
+const SideOptions = () => (
+  <ul className="side-options flex-row">
+    <li className="side-option">About</li>
+    <li className="side-option">Projects</li>
+    <li className="side-option">Skills</li>
+    <li className="side-option">Contact</li>
+  </ul>
+)
+
+export default SideOptions;

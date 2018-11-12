b1f4562050dd22c51d30782b48329fcc16d91d49
Guillermo Barila <gui365@yahoo.com.ar>
2018-11-12 12:33:11 -0300

    Header, font, about

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,20 +3,23 @@ import "./Header.css";
 import "../../App.css";
 
 const Header = () => (
-  <nav className="top-to-bottom header flex-row align-center justify-between">
-    <div className="brand-div flex-row align-center">
-      <div className="logo-div flex-row align-center justify-center">
-        <img className="header-logo" src="./images/gb-logo-tr.png" alt="Guillermo's logo"/>
+  <div className="header">
+    <nav className="top-to-bottom flex-row align-center justify-between">
+      <div className="brand-div flex-row align-center">
+        <div className="logo-div flex-row align-center justify-center">
+          <img className="header-logo" src="./images/gb-logo-tr.png" alt="Guillermo's logo"/>
+        </div>
+        <h3 className="header-name">Guillermo Barila</h3>
       </div>
-      <h3 className="header-name">Guillermo Barila</h3>
-    </div>
-    <ul className="header-options flex-row">
-      <li className="header-option">About</li>
-      <li className="header-option">Projects</li>
-      <li className="header-option">Skills</li>
-      <li className="header-option">Contact</li>
-    </ul>
-  </nav>
+      <ul className="header-options flex-row">
+        <li className="header-option">About</li>
+        <li className="header-option">Projects</li>
+        <li className="header-option">Skills</li>
+        <li className="header-option">Contact</li>
+      </ul>
+    </nav>
+    <div className="border-bottom"></div>
+  </div>
 )
 
 export default Header;
